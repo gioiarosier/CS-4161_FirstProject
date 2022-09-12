d2735613e1e73ccdc3fbb08449c5aab1e950a535
gioia <gioiarosier@gmail.com>
2022-09-12 17:34:38 -0400

    Almost done with login page, needs constraints adjusted

--- app/src/main/java/com/example/mobileproj/LoginActivity.java
+++ app/src/main/java/com/example/mobileproj/LoginActivity.java
@@ -1,16 +1,22 @@
 package com.example.mobileproj;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.airbnb.lottie.Lottie;
 import com.airbnb.lottie.LottieAnimationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.tabs.TabLayout;
 
 public class LoginActivity extends AppCompatActivity {
     LottieAnimationView fruit;
     FloatingActionButton google, fb, twt;
+    ViewPager view;
+    TextView header;
+    //TabLayout tab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,12 +27,18 @@ public class LoginActivity extends AppCompatActivity {
         google = findViewById(R.id.googlebutton);
         fb = findViewById(R.id.fbbutton);
         twt = findViewById(R.id.twitterbutton);
+        header = findViewById(R.id.textView);
+        //tab = findViewById(R.id.tabLayout);
+        view = findViewById(R.id.viewPager);
 
-        google.animate().translationY(-200).setDuration(1000).setStartDelay(1000);
-        fb.animate().translationY(-200).setDuration(1000).setStartDelay(1500);
-        twt.animate().translationY(-200).setDuration(1000).setStartDelay(2000);
-        google.animate().translationY(0).setDuration(1000).setStartDelay(2100);
-        fb.animate().translationY(0).setDuration(1000).setStartDelay(2600);
-        twt.animate().translationY(0).setDuration(1000).setStartDelay(3100);
+//        tab.addTab(tab.newTab().setText("Login"));
+//        tab.addTab(tab.newTab().setText("Sign Up"));
+//        tab.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        //view.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tab));
+
+
+
+
     }
 }
