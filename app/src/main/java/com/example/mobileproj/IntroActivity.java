7880e9e2ac6a2664e6c7326717e44e8c7ffc6a66
gioia <gioiarosier@gmail.com>
2022-09-12 10:54:10 -0400

    login page started

--- app/src/main/java/com/example/mobileproj/IntroActivity.java
+++ app/src/main/java/com/example/mobileproj/IntroActivity.java
@@ -2,7 +2,9 @@ package com.example.mobileproj;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -30,5 +32,14 @@ public class IntroActivity extends AppCompatActivity {
         fruit.animate().translationY(1600).setDuration(1000).setStartDelay(4000);
         basket.animate().translationY(1600).setDuration(1000).setStartDelay(4000);
         lottieAnimationView.animate().translationY(1600).setDuration(1000).setStartDelay(4000);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(IntroActivity.this, LoginActivity.class);
+                startActivity(i);
+                //overridePendingTransition(R.anim.slide_in,R.anim.slide_out);
+            }
+        }, 6000);
     }
 }
--- app/src/main/java/com/example/mobileproj/LoginActivity.java
+++ app/src/main/java/com/example/mobileproj/LoginActivity.java
@@ -4,11 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.airbnb.lottie.Lottie;
+import com.airbnb.lottie.LottieAnimationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class LoginActivity extends AppCompatActivity {
+    LottieAnimationView fruit;
+    FloatingActionButton google, fb, twt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        fruit = findViewById(R.id.avocadoAnimation);
+        google = findViewById(R.id.googlebutton);
+        fb = findViewById(R.id.fbbutton);
+        twt = findViewById(R.id.twitterbutton);
+
+        google.animate().translationY(-200).setDuration(1000).setStartDelay(1000);
+        fb.animate().translationY(-200).setDuration(1000).setStartDelay(1500);
+        twt.animate().translationY(-200).setDuration(1000).setStartDelay(2000);
+        google.animate().translationY(0).setDuration(1000).setStartDelay(2100);
+        fb.animate().translationY(0).setDuration(1000).setStartDelay(2600);
+        twt.animate().translationY(0).setDuration(1000).setStartDelay(3100);
     }
 }
